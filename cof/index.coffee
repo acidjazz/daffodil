ccacf7c52397f2aedca875b4b49801a33aa2522f
kevin olson <acidjazz@gmail.com>
2015-10-21 21:51:32 -0700

    we paralaxxin yet

--- cof/index.coffee
+++ cof/index.coffee
@@ -1,6 +1,29 @@
 Index =
 
+  laxInterval: false
+  cache: {}
+
   i: ->
 
+    Index.cache.banner = $ '.banner'
+    Index.cache.tags = $ '.tags'
+    Index.cache.bcoords = Index.cache.banner[0].getBoundingClientRect()
+
+    console.log Index.cache.bcoords
+
     console.log 'Index.i()'
+
+    Index.laxInterval = setInterval ->
+      Index.lax()
+    , 20
+
+
+  lax: ->
+
+    st = $(window).scrollTop()
+
+    Index.cache.tags.css 'transform', "translate(#{st/7}px, #{st/5}px) rotate(-#{st/20}deg)"
+    Index.cache.banner.css 'transform', "translate(0px, #{st/10}px)"
+
+    #console.log 'paralaxxxxin', st, st/10
 
--- pub/jst/index.js
+++ pub/jst/index.js
@@ -1,7 +1,22 @@
 var Index;
 
 Index = {
+  laxInterval: false,
+  cache: {},
   i: function() {
-    return console.log('Index.i()');
+    Index.cache.banner = $('.banner');
+    Index.cache.tags = $('.tags');
+    Index.cache.bcoords = Index.cache.banner[0].getBoundingClientRect();
+    console.log(Index.cache.bcoords);
+    console.log('Index.i()');
+    return Index.laxInterval = setInterval(function() {
+      return Index.lax();
+    }, 20);
+  },
+  lax: function() {
+    var st;
+    st = $(window).scrollTop();
+    Index.cache.tags.css('transform', "translate(" + (st / 7) + "px, " + (st / 5) + "px) rotate(-" + (st / 20) + "deg)");
+    return Index.cache.banner.css('transform', "translate(0px, " + (st / 10) + "px)");
   }
 };
