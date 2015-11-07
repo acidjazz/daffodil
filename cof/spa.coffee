3b539f3dfd655bd0e1d3d94cd5bb45f989ae128c
kevin olson <acidjazz@gmail.com>
2015-11-06 22:27:41 -0800

    sweet custom loader bars

--- cof/spa.coffee
+++ cof/spa.coffee
@@ -9,7 +9,11 @@ Spa =
 
     Spa.handlers()
 
-    Spa.menu 'home'
+
+    if Object.keys(Spa.sections).indexOf(location.hash.replace('#','')) isnt -1
+      Spa.menu location.hash.replace '#', ''
+    else
+      Spa.menu 'home'
 
   handlers: ->
 
@@ -31,6 +35,52 @@ Spa =
     window[Spa.sections[current]].d() if current isnt ''
 
     _.off ".#{current}.section" if current isnt ''
-    _.on ".#{to}.section"
 
-    window[Spa.sections[to]].i()
+    _.on '.loader'
+    srces = Spa.srces $(".#{to}.section div")
+    Spa.preload srces,
+      (progress) ->
+        $('.loader > .inner > .full').css 'width', "#{progress*100}%"
+        console.log progress
+      , (complete) ->
+        $('.loader > .inner > .full').css 'width', '100%'
+        _.off '.loader'
+        _.on ".#{to}.section"
+        setTimeout ->
+          $('.loader > .inner > .full').css 'width', '0%'
+        , 1000
+
+      window[Spa.sections[to]].i()
+
+  srces: (el) ->
+    srces = []
+    el.each  (i, el) ->
+      src = Spa.srcFromStyle $(el)
+      if src isnt false and src isnt undefined
+        srces.push src
+    return srces
+
+  srcFromStyle: (el) ->
+    style = el.attr 'style'
+    return false if style is undefined
+    url = style.match(/url\((.*)\)/)
+    if url isnt null and url[1] isnt undefined
+      return url[1]
+    else
+      console.log 'ERROR cannot find style for element', style
+      console.log url
+
+  preload: (srces, progress, complete) ->
+
+    images = []
+    loaded = 0
+    total = srces.length
+
+    for src, i in srces
+      images[i] = new Image()
+      images[i].src = src
+      images[i].onload = ->
+        loaded++
+        perc = Math.round(loaded/total*100)/100
+        if loaded is total then complete(true) else progress(perc)
+
--- pub/jst/spa.js
+++ pub/jst/spa.js
@@ -8,7 +8,11 @@ Spa = {
   i: function() {
     console.log('SPA initiation');
     Spa.handlers();
-    return Spa.menu('home');
+    if (Object.keys(Spa.sections).indexOf(location.hash.replace('#', '')) !== -1) {
+      return Spa.menu(location.hash.replace('#', ''));
+    } else {
+      return Spa.menu('home');
+    }
   },
   handlers: function() {
     return $('.header > .menu > a.item').click(Spa.menuHandler);
@@ -17,7 +21,7 @@ Spa = {
     return Spa.menu($(this).text());
   },
   menu: function(to) {
-    var current;
+    var current, srces;
     current = $('.header > .menu > a.item.on').text();
     console.log(current);
     console.log(current + " -> " + to);
@@ -29,7 +33,67 @@ Spa = {
     if (current !== '') {
       _.off("." + current + ".section");
     }
-    _.on("." + to + ".section");
-    return window[Spa.sections[to]].i();
+    _.on('.loader');
+    srces = Spa.srces($("." + to + ".section div"));
+    return Spa.preload(srces, function(progress) {
+      $('.loader > .inner > .full').css('width', (progress * 100) + "%");
+      return console.log(progress);
+    }, function(complete) {
+      $('.loader > .inner > .full').css('width', '100%');
+      _.off('.loader');
+      _.on("." + to + ".section");
+      return setTimeout(function() {
+        return $('.loader > .inner > .full').css('width', '0%');
+      }, 1000);
+    }, window[Spa.sections[to]].i());
+  },
+  srces: function(el) {
+    var srces;
+    srces = [];
+    el.each(function(i, el) {
+      var src;
+      src = Spa.srcFromStyle($(el));
+      if (src !== false && src !== void 0) {
+        return srces.push(src);
+      }
+    });
+    return srces;
+  },
+  srcFromStyle: function(el) {
+    var style, url;
+    style = el.attr('style');
+    if (style === void 0) {
+      return false;
+    }
+    url = style.match(/url\((.*)\)/);
+    if (url !== null && url[1] !== void 0) {
+      return url[1];
+    } else {
+      console.log('ERROR cannot find style for element', style);
+      return console.log(url);
+    }
+  },
+  preload: function(srces, progress, complete) {
+    var i, images, j, len, loaded, results, src, total;
+    images = [];
+    loaded = 0;
+    total = srces.length;
+    results = [];
+    for (i = j = 0, len = srces.length; j < len; i = ++j) {
+      src = srces[i];
+      images[i] = new Image();
+      images[i].src = src;
+      results.push(images[i].onload = function() {
+        var perc;
+        loaded++;
+        perc = Math.round(loaded / total * 100) / 100;
+        if (loaded === total) {
+          return complete(true);
+        } else {
+          return progress(perc);
+        }
+      });
+    }
+    return results;
   }
 };
