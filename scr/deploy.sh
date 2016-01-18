9024ff4493d975c960b7091487f801dd6eab36cf
kevin olson <acidjazz@gmail.com>
2016-01-17 17:16:32 -0800

    mistype

--- scr/deploy.sh
+++ scr/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-s3cmd --config=/Users/k/.s3daffodilcfg sync pub/. s3://daffodildigital.com
+s3cmd --config=/Users/k/.s3daffodilcfg sync pub/. s3://daffodildigital.com --exlcude '.DS_Store'
 s3cmd setacl --config=/Users/k/.s3daffodilcfg s3://daffodildigital.com/ --acl-public --recursive
