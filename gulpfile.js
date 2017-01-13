3c24b11a3f1f57212b7cab39ad7ac0a36c41571d
kevin olson <acidjazz@gmail.com>
2017-01-12 17:46:09 -0800

    typo fix and copy changes

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,6 @@ var sourcemaps = require('gulp-sourcemaps');
 
 var path = require('path');
 var fs = require('fs');
-var cp = require('child_process');
 var gutil = require('gulp-util');
 
 var objectus = require('objectus');
@@ -110,72 +109,9 @@ gulp.task('sync', function() {
   gulp.watch('cof/**/*.coffee', ['coffee']);
   gulp.watch('sty/**/*.styl', ['stylus']);
   gulp.watch('tpl/**/*.jade', ['jade']);
-  gulp.start('mogrify:start');
 
 });
 
-gulp.task('mogrify:start', function() {
-
-  if (!imgwatch) {
-    imgwatch = gulp.watch('pub/img/lrg/**/*');
-    imgwatch.on('change', mogrify);
-  }
-
-});
-
-gulp.task('mogrify:stop', function() {
-
-  if (imgwatch) {
-    imgwatch.end();
-    imgwatch = null;
-   }
-
-});
-
-
-function mogrify(data) {
-
-  gulp.start('mogrify:stop');
-
-  var images = ['.jpeg','.jpg','.png'];
-  var sizes = ['2880','1440','1000'];
-
-  if (['added','changed', 'renamed'].indexOf(data.type) != -1) {
-
-    if (path.extname(data.path).length > 0 && images.indexOf(path.extname(data.path)) != -1) {
-
-      dirname = path.dirname(data.path);
-
-      for (var i = 0, l = sizes.length; i < l; i++) {
-
-        if (dirname.match(sizes[i]) !== null) {
-          gutil.log(gutil.colors.red('Image size matched in dir name, aborting'), dirname);
-          return true;
-        }
-
-        // check if directory exists, if it doesnt, make it
-        var sizeDir = dirname + '/' + sizes[i];
-
-        if (fs.existsSync(sizeDir) === false) {
-          gutil.log('Making directory', '\'' + gutil.colors.cyan(sizeDir) + '\'');
-          cp.execSync('mkdir ' + sizeDir);
-        }
-
-        cp.execSync('cp ' + data.path + ' ' + sizeDir + '/');
-
-        cp.execSync('mogrify -geometry ' + sizes[i] + 'x ' + sizeDir + '/' + path.basename(data.path));
-        gutil.log('Scaling image ', '\'' + gutil.colors.cyan(path.basename(data.path)) + '\'', 'to ' + gutil.colors.magenta(sizes[i] + 'x'));
-
-      }
-
-    }
-
-  }
-
-  gulp.start('mogrify:start');
-  return true;
-
-}
 
 gulp.task('watch', function() {
   gulp.watch('dat/**/*', ['objectus','stylus','jade']);
